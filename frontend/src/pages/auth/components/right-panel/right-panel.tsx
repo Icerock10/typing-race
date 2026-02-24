ed35d61d41d0ce16b8b484f5dde8dc3622a94a8b
Maksym Doroshenko <dp021190dms@gmail.com>
2026-02-24 10:34:22 +0100

    feat: + left auth panel tr-1

---
+++ frontend/src/pages/auth/components/right-panel/right-panel.tsx
@@ -0,0 +1,7 @@
+import styles from './styles.module.css';
+
+const RightPanel: React.FC = () => {
+    return <div className={styles['auth-right-panel']}>Right Panel</div>;
+};
+
+export { RightPanel };
