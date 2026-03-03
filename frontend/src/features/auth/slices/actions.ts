6f06170fa3189cdc992637141231d6247e61d8cb
Maksym Doroshenko <dp021190dms@gmail.com>
2026-03-03 13:53:25 +0100

    fix: * error handle logic tr-8

--- frontend/src/features/auth/slices/actions.ts
+++ frontend/src/features/auth/slices/actions.ts
@@ -1,18 +1,29 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-
-import { ErrorMessage } from '~/libs/enums/enums.js';
+import { ErrorMessage, HTTPError } from '~/libs/enums/enums.js';
 import { StorageKey } from '~/libs/modules/storage/storage.js';
 import {
     type AsyncThunkConfig,
     type UserDto,
     type UserSignInRequestDto,
     type UserSignUpRequestDto,
+    type ServerErrorDetail,
 } from '~/libs/types/types.js';
 
 import {
     actions as authSliceActions,
     name as sliceName,
 } from './auth.slice.js';
+
+type ErrorPayload = {
+    details: ServerErrorDetail[];
+    message: string;
+};
+
+const displayErrorMessage = (error: unknown): ErrorPayload => {
+    return error instanceof HTTPError
+        ? { details: error.details, message: error.message }
+        : { details: [], message: ErrorMessage.DEFAULT_ERROR_MESSAGE };
+};
 
 const signIn = createAsyncThunk<
     UserDto,
@@ -28,8 +39,10 @@ const signIn = createAsyncThunk<
             await storage.set(StorageKey.TOKEN, token);
 
             return user;
-        } catch {
-            return rejectWithValue(ErrorMessage.INVALID_CREDENTIALS);
+        } catch (error) {
+            const { message } = displayErrorMessage(error);
+
+            return rejectWithValue(message);
         }
     },
 );
@@ -38,20 +51,28 @@ const signUp = createAsyncThunk<
     UserDto,
     UserSignUpRequestDto,
     AsyncThunkConfig
->(`${sliceName}/sign-up`, async (registerPayload, { extra }) => {
-    const { authApi, storage } = extra;
+>(
+    `${sliceName}/sign-up`,
+    async (registerPayload, { extra, rejectWithValue }) => {
+        const { authApi, storage } = extra;
 
-    const { token, user } = await authApi.signUp(registerPayload);
-    await storage.set(StorageKey.TOKEN, token);
+        try {
+            const { token, user } = await authApi.signUp(registerPayload);
+            await storage.set(StorageKey.TOKEN, token);
 
-    return user;
-});
+            return user;
+        } catch (error) {
+            const { message } = displayErrorMessage(error);
+            return rejectWithValue(message);
+        }
+    },
+);
 
 const getCurrentUser = createAsyncThunk<
     null | UserDto,
     undefined,
     AsyncThunkConfig
->(`${sliceName}/get-current-user`, async (_, { extra }) => {
+>(`${sliceName}/get-current-user`, async (_, { extra, rejectWithValue }) => {
     const { authApi, storage } = extra;
 
     const token = await storage.get(StorageKey.TOKEN);
@@ -60,18 +81,23 @@ const getCurrentUser = createAsyncThunk<
         return null;
     }
 
-    return await authApi.getCurrentUser();
+    try {
+        return await authApi.getCurrentUser();
+    } catch (error) {
+        const { message } = displayErrorMessage(error);
+        return rejectWithValue(message);
+    }
 });
 
 const logout = createAsyncThunk<null, undefined, AsyncThunkConfig>(
     `${sliceName}/logout`,
-    async (_payload, { dispatch, extra }) => {
+    async (_payload, { dispatch, extra, rejectWithValue }) => {
         const { storage } = extra;
 
         try {
             await storage.drop(StorageKey.TOKEN);
         } catch {
-            throw new Error(ErrorMessage.DEFAULT_ERROR_MESSAGE);
+            return rejectWithValue(ErrorMessage.DEFAULT_ERROR_MESSAGE);
         }
 
         dispatch(authSliceActions.resetAuthState());
