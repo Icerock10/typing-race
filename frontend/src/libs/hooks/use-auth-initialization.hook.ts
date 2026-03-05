241fd5d6fc49c12a8addb0ee8614397b64220acb
Maksym Doroshenko <dp021190dms@gmail.com>
2026-03-05 17:37:27 +0100

    feat: + implemented discord login on client tr-9

--- frontend/src/libs/hooks/use-auth-initialization.hook.ts
+++ frontend/src/libs/hooks/use-auth-initialization.hook.ts
@@ -2,13 +2,16 @@ import {
     useAppDispatch,
     useAppSelector,
     useCallback,
+    useLocation,
     useEffect,
 } from '~/libs/hooks/hooks.js';
 import { actions as authActions } from '~/features/auth/auth.js';
+import { StorageKey, storage } from '../modules/storage/storage.js';
 
 const useAuthInitialization = (): void => {
     const dispatch = useAppDispatch();
     const { user } = useAppSelector((state) => state.auth);
+    const location = useLocation();
 
     const initializeAuth = useCallback((): void => {
         if (user) {
@@ -19,6 +22,12 @@ const useAuthInitialization = (): void => {
     }, [dispatch, user]);
 
     useEffect(() => {
+        const parameters = new URLSearchParams(location.search);
+        const token = parameters.get(StorageKey.TOKEN);
+        if (token) {
+            void storage.set(StorageKey.TOKEN, token);
+        }
+
         initializeAuth();
     }, [initializeAuth]);
 };
