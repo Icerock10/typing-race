f085cbd8403bf88a108fe664b777e72211efeafd
Maksym Doroshenko <dp021190dms@gmail.com>
2026-03-07 12:59:37 +0100

    feat: + create room logic, auth on handshake tr-10

---
+++ backend/src/features/game-store/types/types.ts
@@ -0,0 +1,14 @@
+import { type RoomResponseDto, type UserDto } from '~/libs/types/types.js';
+
+type Player = {
+    socketId: string;
+    user: UserDto;
+};
+
+type InternalRoom = Omit<RoomResponseDto, 'players'> & {
+    players: Map<string, Player>;
+};
+
+export { type InternalRoom, type Player };
+
+export { type RoomResponseDto } from '~/libs/types/types.js';
