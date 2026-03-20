a7944a1eaf14690ef9121d9d03fa8212a2d159ce
Maksym Doroshenko <dp021190dms@gmail.com>
2026-03-20 15:50:41 +0100

    refactor: + lobby-service layer to reorganize logic tr-28

--- backend/src/features/game/handlers/lobby/lobby.ts
+++ backend/src/features/game/handlers/lobby/lobby.ts
@@ -1,9 +1,12 @@
 import { type DepHandlers } from '../../libs/types/types.js';
 import { type ChatHandler } from '../chat/chat-handler.module.js';
 import { LobbyHandler } from './lobby-handler.module.js';
+import { LobbyService } from './lobby-service.module.js';
 
 const initLobby = (deps: DepHandlers & { chat: ChatHandler }): void => {
-    new LobbyHandler(deps);
+    const { store, userService, chat, socket, io } = deps;
+    const lobbyService = new LobbyService({ store, userService, chat });
+    new LobbyHandler({ io, socket, lobbyService });
 };
 
 export { initLobby };
