e9aea54afa9768935c60b0f7a0f7667848566b79
Maksym Doroshenko <dp021190dms@gmail.com>
2026-04-28 15:57:56 +0200

    fix: + actual links, app version tr-38

--- frontend/src/libs/components/footer/footer.tsx
+++ frontend/src/libs/components/footer/footer.tsx
@@ -1,26 +1,31 @@
 import styles from './styles.module.css';
 import { Link, Cluster } from '../components.js';
-import { AppRoute } from '~/libs/enums/enums.js';
+import { type AppRoute } from '~/libs/enums/enums.js';
+import { type ValueOf } from '~/libs/types/types.js';
+// eslint-disable-next-line import/extensions
+import manifest from '~/../../.release-please-manifest.json' with { type: 'json' };
 
 const FOOTER_MENU_LINKS = [
     {
-        label: 'About',
-    },
-    {
         label: 'GitHub',
+        path: 'https://github.com/Icerock10',
     },
     {
         label: 'Discord',
+        path: 'https://discord.com/',
     },
 ];
 
 const Footer: React.FC = () => {
     return (
         <footer className={styles['footer']}>
-            <span>typerace v0.1 — made with ⚡</span>
+            <span>typerace v{manifest['.']} — made with ⚡</span>
             <Cluster className={styles['links']}>
                 {FOOTER_MENU_LINKS.map((link) => (
-                    <Link to={AppRoute.ROOT} key={link.label}>
+                    <Link
+                        to={link.path as unknown as ValueOf<typeof AppRoute>}
+                        key={link.label}
+                    >
                         {link.label}
                     </Link>
                 ))}
