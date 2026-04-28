f4256fd6928093cf34cb08bf1daaab38bf85a55a
Maksym Doroshenko <dp021190dms@gmail.com>
2026-04-28 17:53:21 +0200

    feat: + wpm record instead of mock value tr-41

--- frontend/src/libs/components/stats/stats.tsx
+++ frontend/src/libs/components/stats/stats.tsx
@@ -1,11 +1,39 @@
 import styles from './styles.module.css';
-import { useAppSelector } from '~/libs/hooks/hooks.js';
+import { useAppSelector, useMemo } from '~/libs/hooks/hooks.js';
 import { Cluster } from '~/libs/components/components.js';
 import { ClusterVariant } from '~/libs/enums/enums.js';
 import { STATS_CONFIG } from './libs/enums/stats-config.enum.js';
+import { type AppStatsDto } from '~/libs/types/types.js';
+
+const STATS_DEFAULT_VALUE = 0;
 
 const Stats: React.FC = () => {
-    const { stats } = useAppSelector((state) => state.game);
+    const { stats, games } = useAppSelector((state) => state.game);
+    const wpmRecord = useMemo(
+        () =>
+            Math.max.apply(
+                null,
+                games
+                    .flatMap((game) => game.results)
+                    .map((result) => result.wpm),
+            ),
+        [games],
+    );
+
+    const prepareStats = (key: keyof AppStatsDto): number => {
+        switch (key) {
+            case 'activeRooms': {
+                return stats?.[key].length || STATS_DEFAULT_VALUE;
+            }
+            case 'wpm': {
+                return wpmRecord;
+            }
+
+            default: {
+                return stats?.[key] || STATS_DEFAULT_VALUE;
+            }
+        }
+    };
 
     return (
         <Cluster cluster={ClusterVariant.FLEX} className={styles['auth-stats']}>
@@ -15,7 +43,7 @@ const Stats: React.FC = () => {
                     data-stat={dataStat}
                     className={styles['stat-value']}
                 >
-                    {key === 'activeRooms' ? stats?.[key].length : stats?.[key]}
+                    {prepareStats(key)}
                     <p className={styles['stat-label']}>{label}</p>
                 </span>
             ))}
