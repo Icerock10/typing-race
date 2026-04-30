b008f219b0b6a3fc62cffc64322690d732fdb81a
Maksym Doroshenko <dp021190dms@gmail.com>
2026-04-30 12:24:49 +0200

    feat: + placeholder for empty rooms tr-43

--- frontend/src/assets/image/image.ts
+++ frontend/src/assets/image/image.ts
@@ -4,3 +4,4 @@ export { default as RocketIcon } from './rocket.svg?react';
 export { default as ArrowRight } from './arrow-right.svg?react';
 export { default as ArrowDown } from './arrow-down.svg?react';
 export { default as LogoutIcon } from './logout.svg?react';
+export { default as PlusIcon } from './plus.svg?react';
