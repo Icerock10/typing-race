18c37b0990576991b09e5b3ec5624d39d58b31ad
Maksym Doroshenko <dp021190dms@gmail.com>
2026-04-30 11:51:07 +0200

    style: + icons tr-42

--- frontend/src/assets/image/image.ts
+++ frontend/src/assets/image/image.ts
@@ -1,3 +1,6 @@
 export { default as LinkIcon } from './link.svg?react';
 export { default as CrossIcon } from './x.svg?react';
 export { default as RocketIcon } from './rocket.svg?react';
+export { default as ArrowRight } from './arrow-right.svg?react';
+export { default as ArrowDown } from './arrow-down.svg?react';
+export { default as LogoutIcon } from './logout.svg?react';
